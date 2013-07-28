a8b228229e295cb12b29c7a473bad7bb722a69a9
lonegunman <lonegunmanb@hotmail.com>
2013-07-28 17:21:32 +0800

    添加一个测试

---
+++ ElasticSearch.Tests/DateTimeSerializeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ElasticSearch.Client.Utils;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ElasticSearch.Tests
+{
+    [TestFixture]
+    public class DateTimeSerializeTest
+    {
+        [Test]
+        public void DateTimeJsonSerializeTest()
+        {
+            var str = JsonBuilder.strValueOf(DateTime.Now, false);
+
+            var result = JsonConvert.DeserializeObject(str, typeof(DateTime), new JsonSerializerSettings()
+            {
+                //			ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore,
+                DefaultValueHandling = DefaultValueHandling.Ignore,
+                Converters = new List<JsonConverter> { new IsoDateTimeConverter() }
+            });
+        }
+    }
+}
